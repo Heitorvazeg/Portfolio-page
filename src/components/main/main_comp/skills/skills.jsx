27383a0731dec5fe173a8cb56368d9fc50dd7987
Heitorvazeg <heitorvazeg@gmail.com>
2025-06-03 17:14:57 -0300

    desenvolvendo...

--- src/components/main/main_comp/skills/skills.jsx
+++ src/components/main/main_comp/skills/skills.jsx
@@ -5,11 +5,51 @@ function Skills() {
     return (
         <section id="skills">
             <div className="blocoSkills">
-                <h1>Skills</h1>
+                <h1 className='sk'>Skills</h1>
                 <p></p>
-                <div className="skill">
-                    <img src="" alt="" />
-                    <p></p>
+                <div className="skillBloco">
+                    <div className="ternarioSkill">
+                        <div className="skill">
+                            <img src="" alt="" />
+                            <p>React.js</p>
+                        </div>
+                        <div className="skill">
+                            <img src="" alt="" />
+                            <p>Go</p>
+                        </div>
+                        <div className="skill">
+                            <img src="" alt="" />
+                            <p>Javascript</p>
+                        </div>
+                    </div>
+                    <div className="ternarioSkill">
+                        <div className="skill">
+                            <img src="" alt="" />
+                            <p>Python</p>
+                        </div>
+                        <div className="skill">
+                            <img src="" alt="" />
+                            <p>C++</p>
+                        </div>
+                        <div className="skill">
+                            <img src="" alt="" />
+                            <p>HTML/CSS</p>
+                        </div>
+                    </div>
+                    <div className="ternarioSkill">
+                        <div className="skill">
+                            <img src="" alt="" />
+                            <p>SQL</p>
+                        </div>
+                        <div className="skill">
+                            <img src="" alt="" />
+                            <p>APIs REST</p>
+                        </div>
+                        <div className="skill">
+                            <img src="" alt="" />
+                            <p>Git/Github</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
